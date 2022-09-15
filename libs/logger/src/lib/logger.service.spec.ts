def3f9ae36ed5e7057036883e5a80c4849354742
Arne Seidenfaden <arne.seidenfaden@konvoj.de>
2022-09-15 13:44:22 +0300

    Final

--- libs/logger/src/lib/logger.service.spec.ts
+++ libs/logger/src/lib/logger.service.spec.ts
@@ -1,16 +1,31 @@
 import { TestBed } from '@angular/core/testing';
+import { BaseLoggingServiceService } from './base-logging-service.service';
+import { ConsoleLoggerService } from './console-logger.service';
 
 import { LoggerService } from './logger.service';
+import { LoggerConfig } from './models';
 
-describe('LoggerService', () => {
+fdescribe('LoggerService', () => {
   let service: LoggerService;
-
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: BaseLoggingServiceService,
+          useClass: ConsoleLoggerService,
+          multi: true
+        },
+        { provide: LoggerConfig, useValue: LoggerConfig }
+      ]
+    });
     service = TestBed.inject(LoggerService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  it('should log an error', () => {
+    service.logError(new Error('Test Error'));
   });
 });
